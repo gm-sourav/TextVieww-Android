191c967b45b86050fcd77565ac0e8195a839db78
gm-sourav <souravmahbub13@gmail.com>
2026-08-15 01:12:25 +0600

    Next

--- app/src/main/java/com/example/txtview/MainActivity.kt
+++ app/src/main/java/com/example/txtview/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.txtview
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nameTextView : TextView;
+    private lateinit var ageTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        nameTextView = findViewById(R.id.nametextViewId)
+        ageTextView = findViewById(R.id.agetextViewId)
+        nameTextView.text = "Mitua Afrin Prithibi"
+        ageTextView.text = "23 years old"
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
